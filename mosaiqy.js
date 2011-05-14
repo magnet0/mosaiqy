fb2e5c32847f4caa8e4d54a1106b63f3219d19d0
fcalderan <fabrizio.calderan@gmail.com>
2011-05-14 17:27:24 +0200

    License updated

--- mosaiqy.js
+++ mosaiqy.js
@@ -3,6 +3,10 @@
  * @fileOverview Mosaiqy for jQuery
  * @version 0.1
  * @author Fabrizio Calderan, http://www.fabriziocalderan.it/, twitter : fcalderan
+ *
+ * Released under license Creative Commons - Attribution-NoDerivs 3.0
+ * (CC BY-ND 3.0) available at http://creativecommons.org/licenses/by-nd/3.0/before
+ * Read the license carefully before using this plugin.  
  */
 
 (function($) {
