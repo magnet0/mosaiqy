f6fb620c04dac4e5f8d7186ce6916db3cd9aeed8
Fabrizio Calderan <fabrizio.calderan@gmail.com>
2011-05-25 18:15:58 +0200

    title of templates moved as server inclusion

--- mosaiqy-flickr.php
+++ mosaiqy-flickr.php
@@ -14,18 +14,7 @@
 
 <body>
 
-    <header>
-        <hgroup>
-            <h1>Mosaiqy</h1>
-            <h2>A nice plugin for <em>jQuery 1.6+</em> &amp; <em>HTML5</em></h2>
-            <h3>(<em>8Kb</em> minified - <em>3.6Kb</em> minified + gzip)</h3>
-            <!-- yes I am an hgroup supporter -->
-        </hgroup>
-            
-        <p class="github-ribbon">
-           <a href="https://github.com/fcalderan/mosaiqy" target="new">Hosted on Github</a>
-        </p>
-    </header>
+    <?php require "includes/title.php" ?>
     
     <div class="loading mosaiqy">
         <ul>
