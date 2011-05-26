92f6e24ce8b43a73e9f1ee470b5fcd8c548ef011
Fabrizio Calderan <fabrizio.calderan@gmail.com>
2011-05-26 14:00:28 +0200

    deleted duplicate file

--- includes/jquery.php
+++
@@ -1,5 +0,0 @@
-    
-	<!-- jquery & lib-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"> </script>
-    <script>!window.jQuery && document.write('<'+'script src="http://static.fabriziocalderan.it/js/jquery-1.6.1.min.js"> </'+'script>'); </script>
-    <script src="lib/mosaiqy-1.0.0.js" id="mosaiqy_tpl">
