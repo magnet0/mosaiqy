b857d96e42a005bdc12b32eb3c09200c6fd43140
fcalderan <fabrizio.calderan@gmail.com>
2011-06-09 21:49:32 +0200

    working on user documentation

--- index.php
+++ index.php
@@ -151,21 +151,21 @@
         script</a> if you need to use specific HTML5 elements (and if you also plan to run this plugin on IE versions prior to 9).
         </p>
         
-        <code><pre>
+        <pre><code>
 &lt;!doctype html&gt;
 &lt;!--[if lt IE 7]&gt; &lt;html class="no-js ie6" lang="en"&gt; &lt;![endif]--&gt;
 &lt;!--[if IE 7]&gt;    &lt;html class="no-js ie7" lang="en"&gt; &lt;![endif]--&gt;
 &lt;!--[if IE 8]&gt;    &lt;html class="no-js ie8" lang="en"&gt; &lt;![endif]--&gt;
 &lt;!--[if gt IE 8]&gt;&lt;!--&gt;  &lt;html class="no-js" lang="en"&gt; &lt;!--&lt;![endif]--&gt;
 &lt;head&gt;
-    ...</pre></code>
+    ...</code></pre>
 
         <p>
             if you do not even load modernizr into your page, please make sure to insert this
             snippet soon into your <code class="inline">&lt;head&gt;</code> section:
         </p>
         
-        <code><pre>
+        <pre><code>
 &lt;head&gt;
     &lt;!--[if lt IE 9]&gt;
         &lt;script src="http://html5shim.googlecode.com/svn/trunk/html5.js"&gt;&lt;/script&gt;
@@ -176,17 +176,17 @@
         }(document.documentElement));
     &lt;/script&gt;
     ...
-&lt;/head&gt;</pre></code>
+&lt;/head&gt;</code></pre>
         
         <p>
         Finally, simply include jQuery and the javascript library (better at the bottom of your document) and the css file:</p>
 
-        <code><pre>
+        <pre><code>
 &lt;link rel="stylesheet" media="screen" href="lib/lib-css/mosaiqy.css" /&gt;
 ...
 &lt;script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"&gt; &lt;/script&gt;
 &lt;script src="lib/mosaiqy-1.0.0.js"&gt; &lt;/script&gt;
-        </pre></code>
+        </code></pre>
         
         <p>Now you're ready to use mosaiqy on you page. For any doubt you are encouraged to take a look at
         the source code of the demo pages.</p>
